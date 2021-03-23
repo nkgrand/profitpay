faddd09402f789ce0442ffb16765a159744ffaa6
Andrey Grigoriev <nkgrand25@gmail.com>
2021-03-23 14:32:11 +0200

    made new js for person click

--- js/personClick.js
+++ js/personClick.js
@@ -1,96 +1,20 @@
-// (() => {
-//   const mainBlock = document.querySelector(".section-team__team-list");
-//   const person1 = document.querySelector("#person1");
-//   const person1Detailed = document.querySelector(".person1_detailed");
-//   const person2 = document.querySelector("#person2");
-//   const person2Detailed = document.querySelector(".person2_detailed");
-//   const person3 = document.querySelector("#person3");
-//   const person3Detailed = document.querySelector(".person3_detailed");
-//   const person4 = document.querySelector("#person4");
-//   const person4Detailed = document.querySelector(".person4_detailed");
-//   const person5 = document.querySelector("#person5");
-//   const person5Detailed = document.querySelector(".person5_detailed");
-//   const person6 = document.querySelector("#person6");
-//   const person6Detailed = document.querySelector(".person6_detailed");
-
-//   person1.addEventListener("click", function () {
-//     mainBlock.style.visibility = "hidden";
-//     person1Detailed.style.visibility = "visible";
-//   });
-
-//   person1Detailed.addEventListener("click", function () {
-//     person1Detailed.style.visibility = "hidden";
-//     mainBlock.style.visibility = "visible";
-//   });
-
-//   person2.addEventListener("click", function () {
-//     mainBlock.style.visibility = "hidden";
-//     person2Detailed.style.visibility = "visible";
-//   });
-
-//   person2Detailed.addEventListener("click", function () {
-//     person2Detailed.style.visibility = "hidden";
-//     mainBlock.style.visibility = "visible";
-//   });
-
-//   person3.addEventListener("click", function () {
-//     mainBlock.style.visibility = "hidden";
-//     person3Detailed.style.visibility = "visible";
-//   });
-
-//   person3Detailed.addEventListener("click", function () {
-//     person3Detailed.style.visibility = "hidden";
-//     mainBlock.style.visibility = "visible";
-//   });
-
-//   person4.addEventListener("click", function () {
-//     mainBlock.style.visibility = "hidden";
-//     person4Detailed.style.visibility = "visible";
-//   });
-
-//   person4Detailed.addEventListener("click", function () {
-//     person4Detailed.style.visibility = "hidden";
-//     mainBlock.style.visibility = "visible";
-//   });
-
-//   person5.addEventListener("click", function () {
-//     mainBlock.style.visibility = "hidden";
-//     person5Detailed.style.visibility = "visible";
-//   });
-
-//   person5Detailed.addEventListener("click", function () {
-//     person5Detailed.style.visibility = "hidden";
-//     mainBlock.style.visibility = "visible";
-//   });
-
-//   person6.addEventListener("click", function () {
-//     mainBlock.style.visibility = "hidden";
-//     person6Detailed.style.visibility = "visible";
-//   });
-
-//   person6Detailed.addEventListener("click", function () {
-//     person6Detailed.style.visibility = "hidden";
-//     mainBlock.style.visibility = "visible";
-//   });
-// })();
-
-function personClick(wrapper) {
-  const el = wrapper.querySelectorAll(".section-team__item");
+(() => {
+  const person = document.querySelectorAll(".section-team__item");
+  const personDetailed = document.querySelectorAll(".person_detailed");
   const mainBlock = document.querySelector(".section-team__team-list");
 
-  for (let i = 0; i < el.length; i++) {
-    personClick(el[i]);
-  }
+  arrOfPerson = Array.from(person);
+  arrOfDetailedPersons = Array.from(personDetailed);
 
-  function personClick(button) {
-    button.addEventListener("click", function () {
-      showPersonDetailed();
+  arrOfPerson.forEach((element, index) => {
+    element.addEventListener("click", function () {
+      mainBlock.style.visibility = "hidden";
+      arrOfDetailedPersons[index].style.visibility = "visible";
+
+      arrOfDetailedPersons[index].addEventListener("click", function () {
+        arrOfDetailedPersons[index].style.visibility = "hidden";
+        mainBlock.style.visibility = "visible";
+      });
     });
-  }
-
-  function showPersonDetailed(id) {
-    // mainBlock.style.visibility = "hidden";
-    console.log("some");
-  }
-}
-personClick(document.querySelector(".section-team__team-list"));
+  });
+})();
