0d436d2374e6adedf696c65221d3f1b37c85f1d4
Andrey Grigoriev <nkgrand25@gmail.com>
2021-03-23 16:29:33 +0200

    deleted console.log

--- js/lines.js
+++ js/lines.js
@@ -1,51 +1,12 @@
 (() => {
-  // const block = document.querySelector(".section-contacts__contacts");
-  // const girl = document.querySelector("#girl");
-  // const boy = document.querySelector("#boy");
-
-  // const windowInnerWidth = window.innerWidth;
-  // console.log(windowInnerWidth);
-
-  // girl.addEventListener("mouseover", mouseOverGirl);
-  // girl.addEventListener("mouseout", mouseOutGirl);
-
-  // boy.addEventListener("mouseover", mouseOverBoy);
-  // boy.addEventListener("mouseout", mouseOutBoy);
-
-  // function mouseOverGirl() {
-  //   if (windowInnerWidth > "955") {
-  //     block.style.backgroundImage = "url('/image/contact-lines-left.svg')";
-  //   }
-  // }
-
-  // function mouseOutGirl() {
-  //   if (windowInnerWidth > "955") {
-  //     block.style.backgroundImage = "url('/image/contact-lines.svg')";
-  //   }
-  // }
-
-  // function mouseOverBoy() {
-  //   if (windowInnerWidth > "955") {
-  //     block.style.backgroundImage = "url('/image/contact-lines-right.svg')";
-  //   }
-  // }
-
-  // function mouseOutBoy() {
-  //   if (windowInnerWidth > "955") {
-  //     block.style.backgroundImage = "url('/image/contact-lines.svg')";
-  //   }
-  // }
-
   window.addEventListener("mouseover", function () {
     const windowInnerWidth = window.innerWidth;
     const block = document.querySelector(".section-contacts__contacts");
     const girl = document.querySelector("#girl");
     const boy = document.querySelector("#boy");
-    console.log(windowInnerWidth);
 
     if (windowInnerWidth > 768) {
       const windowInnerWidth = window.innerWidth;
-      console.log(windowInnerWidth);
 
       girl.addEventListener("mouseover", mouseOverGirl);
       girl.addEventListener("mouseout", mouseOutGirl);
@@ -70,7 +31,6 @@
       }
     }
     if (windowInnerWidth < 768) {
-      console.log("хуй");
       block.style.backgroundImage = "";
     }
   });
