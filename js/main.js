96e1ccbf0d8942e1745136dc6a3131b3f3132f17
Anton Reznichenko <me@wr.su>
2021-03-24 08:43:37 +0300

    Fix: cookie and modal

--- js/main.js
+++ js/main.js
@@ -92,40 +92,40 @@ $(".section-team__team-list").slick({
   ],
 });
 
-// (() => {
-//   const body = document.querySelector(".body");
-//   const modalContainer = document.querySelector(".modal-form");
-//   const modalOverlay = document.querySelector(".overlay");
-//   const cookieContainer = document.querySelector(".section-cookie");
-//   const modalBtnClose = document.querySelector(".btnClose");
-//   const cookiesBtnClose = document.querySelector(".cookiesBtnClose");
+(() => {
+  const body = document.querySelector(".body");
+  const modalContainer = document.querySelector(".modal-form");
+  const modalOverlay = document.querySelector(".overlay");
+  const cookieContainer = document.querySelector(".section-cookie");
+  const modalBtnClose = document.querySelector(".btnClose");
+  const cookiesBtnClose = document.querySelector(".cookiesBtnClose");
 
-//   function displayModal() {
-//     modalOverlay.style.display = "block";
-//     modalContainer.style.display = "block";
-//     body.classList.add("modal-open");
-//   }
+  function displayModal() {
+    modalOverlay.style.display = "block";
+    modalContainer.style.display = "block";
+    body.classList.add("modal-open");
+  }
 
-//   function displayCockies() {
-//     cookieContainer.style.display = "flex";
-//   }
+  function displayCockies() {
+    cookieContainer.style.display = "flex";
+  }
 
-//   setTimeout(displayModal, 1000);
-//   setTimeout(displayCockies, 3000);
+  setTimeout(displayModal, 1000);
+  setTimeout(displayCockies, 3000);
 
-//   modalBtnClose.addEventListener("click", function () {
-//     modalContainer.style.display = "none";
-//     modalOverlay.style.display = "none";
-//     body.classList.remove("modal-open");
-//   });
+  modalBtnClose.addEventListener("click", function () {
+    modalContainer.style.display = "none";
+    modalOverlay.style.display = "none";
+    body.classList.remove("modal-open");
+  });
 
-//   modalOverlay.addEventListener("click", function () {
-//     modalContainer.style.display = "none";
-//     modalOverlay.style.display = "none";
-//     body.classList.remove("modal-open");
-//   });
+  modalOverlay.addEventListener("click", function () {
+    modalContainer.style.display = "none";
+    modalOverlay.style.display = "none";
+    body.classList.remove("modal-open");
+  });
 
-//   cookiesBtnClose.addEventListener("click", function () {
-//     cookieContainer.style.display = "none";
-//   });
-// })();
+  cookiesBtnClose.addEventListener("click", function () {
+    cookieContainer.style.display = "none";
+  });
+})();
